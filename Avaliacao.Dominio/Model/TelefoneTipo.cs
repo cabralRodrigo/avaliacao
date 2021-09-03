aa400f39b2d844620287c7546bf132c1e28976d0
Rodrigo Cabral <rodrigopaivac@gmail.com>
2021-09-03 19:25:57 -0300

    Corrigindo display do tipo de telefone 'Residencial'.

--- Avaliacao.Dominio/Model/TelefoneTipo.cs
+++ Avaliacao.Dominio/Model/TelefoneTipo.cs
@@ -1,10 +1,15 @@
-﻿namespace Avaliacao.Dominio.Model
+﻿using System.ComponentModel;
+
+namespace Avaliacao.Dominio.Model
 {
     public enum TelefoneTipo
     {
         Pessoal = 1,
         Comercial = 2,
+
+        [Description("Residêncial")]
         Residencial = 3,
+
         Outros = 4
     }
 }
